49cc4b1516f57223d43e50f5069dbbce3369a65d
vishalchandrakumar <vishalchandrakumar07@gmail.com>
2024-07-31 16:33:04 +0530

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -114,18 +114,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = "static/"
 MEDIA_URL = "/images/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Enable gzip and Brotli compression
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
